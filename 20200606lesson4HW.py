6c1dec5cfaf0a7d9e64d6c28df0e3439be552b2d
Herbie Chu <65217537+herbiecool@users.noreply.github.com>
2020-06-08 19:13:08 +0800

    Add files via upload

---
+++ 20200606lesson4HW.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  6 13:00:32 2020
+
+@author: redno
+"""
+
+import turtle as herbie
+
+herbie.color('green')
+herbie.title('Herbie')
+herbie.bgcolor('yellow')
+herbie.shape('turtle')
+
+r=10
+
+for i in range(15):
+ 
+   herbie.clone()
+    
+   herbie.circle(r,90)
+   r=r+15
+   
+        
+herbie.done()
+herbie.bye()    
+    
+    
+ 
+
+
+
+
+
+
+
+
