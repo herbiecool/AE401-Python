91aed5de334514858f3965aa661cb71abfca57a8
Herbie Chu <65217537+herbiecool@users.noreply.github.com>
2020-06-30 18:31:58 +0800

    Add files via upload

--- lesson7HW0629.py
+++ lesson7HW0629.py
@@ -9,15 +9,17 @@ name=['小明','小方','小志']
 verb=['飛到','看到','走到']
 noun=['日本。','綠島。','美國。']
 
-a=random.sample(name,1)
-b=random.sample(verb,1)
-c=random.sample(noun,1)
+for i in range(10):
 
-str_a="".join(a)
-str_b="".join(b)
-str_c="".join(c)
+    a=random.sample(name,1)
+    b=random.sample(verb,1)
+    c=random.sample(noun,1)
+    
+    str_a="".join(a)
+    str_b="".join(b)
+    str_c="".join(c)
 
-print('句子:',str_a+str_b+str_c)
+    print('句子:',str_a+str_b+str_c)
 
 
 
